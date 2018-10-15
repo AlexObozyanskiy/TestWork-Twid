3ee25e94f4df99ab722d7637d30583a5d34ae966
Alex Obozyanskiy <alex.obozi@gmail.com>
2018-10-15 16:30:13 +0300

    I hope you like it :)

--- js/script.js
+++ js/script.js
@@ -1,38 +1,19 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 430) {
+        $('.fixed-container').addClass('display-block');
+        $('.header').addClass('fixed-header');
+    }
+    else {
+        $('.fixed-container').removeClass('display-block');
+        $('.header').removeClass('fixed-header');
+    }
+});
+
 $(document).ready(function(){
-	$("#first-page").click(function(e) {
-	  e.preventDefault();
-	  $(".main-page").fadeOut("slow");
-	  setTimeout(
-		  function() 
-		  {
-		    $(".factory-page").fadeIn("slow");
-		  }, 500);
-	});
-	$("#history-more").click(function(e) {
-		e.preventDefault();
-		$("#history-more-container").slideDown(800);
-		$("#history-more").hide();
-
-	});
-	$("#history-more-sec").click(function(e) {
-		e.preventDefault();
-		$("#history-more-container-sec").slideDown(800);
-		$("#history-more-sec").hide();
-
-	});
-	$('.carousel').carousel({
-		interval: 3000
-	});
-	$(".close-button").click(function() {
-		$("#history-more-container").slideUp(800);
-		$("#history-more").show();
-	});
-	$(".close-button-sec").click(function() {
-		$("#history-more-container-sec").slideUp(800);
-		$("#history-more-sec").show();
-	});
-	$(".play-button").click(function() {
-		$("#video-container").html('<iframe class="video-iframe" width="566px" height="555px" src="https://www.youtube.com/embed/WYNkHN7V6lE?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
-	});
-
+ $('.donation-block.mobile .donation-list ul').readmore({
+    speed: 700,
+    maxHeight: 112,
+    moreLink: '<p class="read-more">Смотреть еще <span>36</span></p>',
+    lessLink: '<p class="read-more">Скрыть</p>'
+  });
 });
